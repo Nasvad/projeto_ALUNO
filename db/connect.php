53add6688a43bbda7379def9e9a4db76c0e09fa1
Davi Freire <davifreireangelim@gmail.com>
2021-06-17 17:39:38 -0300

    HEROKU

--- db/connect.php
+++ db/connect.php
@@ -1,10 +1,19 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname   = "projeto_aluno";
+//LOCAL
+  //$servername = "localhost";
+  //$username = "root";
+  //$password = "";
+  //$dbname   = "projeto_aluno";
+
+  //HEROKU
+$servername = "xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "	qhk1cyx98g8k22id";
+$password = "	zgffr5qudwurqol1";
+$dbname   = "d5ppxxl81au16tva";
+
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Conexão bem sucedida!";
