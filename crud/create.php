0c6d9e086b13e16c4ced17c7abceecaf2e8b5039
Davi Freire <davifreireangelim@gmail.com>
2021-06-17 18:06:28 -0300

    HeidiSQL (v0.3)

--- crud/create.php
+++ crud/create.php
@@ -19,7 +19,7 @@ try {
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $sql = "INSERT INTO aluno (nomeAluno, nota1, nota2)
-  VALUES ('Maria', '8.4', '6.6')";
+  VALUES ('Eunice', '4.4', '9.6')";
   // use exec() because no results are returned
   $conn->exec($sql);
   echo "Aluno cadastrado com sucesso!";
