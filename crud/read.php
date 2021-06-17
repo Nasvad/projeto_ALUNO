d63b5058656ff517b7ba88dae03a36fb7d871afc
Davi Freire <davifreireangelim@gmail.com>
2021-06-17 18:16:22 -0300

    beta version

---
+++ crud/read.php
@@ -0,0 +1,52 @@
+<?php
+
+
+
+// Dados de conexão LOCAL
+//$servername = "localhost";
+//$username   = "root";
+//$password   = "";
+//$dbname     = "blog";
+
+//Dados de conexão HEROKU
+$servername = "xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "qhk1cyx98g8k22id";
+$password = "zgffr5qudwurqol1";
+$dbname   = "d5ppxxl81au16tva";
+  
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->prepare("SELECT * FROM users");
+  $stmt->execute();
+
+  // set the resulting array to associative
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  foreach($stmt->fetchAll() as $k=>$v) {
+    echo '<tr>';
+    echo '<td>'.$v['id'].'</td>';
+    echo '<td>'.$v['nomeAluno'].'</td>';
+    echo '<td>'.$v['nota1'].'</td>';
+    echo '<td>'.$v['nota2'].'</td>';
+    echo 
+    '<td> 
+
+
+      <a class="btn btn-outline-danger" href="crud/delete.php?id='. $v['id']. '" >   <i class="far fa-trash-alt"></i>    </a> 
+
+
+
+
+      <a class="btn btn-outline-primary" href="#" >   <i class="far fa-edit"></i>        </a>   
+    </td>';
+    echo '</tr>';
+  }
+} catch(PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>
--- index.php
+++ index.php
@@ -4,24 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Atividade PHP</title>
+    <title>Projeto ALUNO</title>
 </head>
 <body>
     <?php
         echo "Praticando conexão com Banco de Dados MySQL <br>";
     ?>
 
-    <h2>Códigos BackEnd</h2>
+    <h2>Comandos</h2>
     <a href="crud/create.php">Create</a>
     <a href="crud/read.php">Read</a>
-    <a href="crud/update.php">Update</a>
-    <a href="crud/delete.php">Delete</a>
-    <br>
-
-    <h2>Páginas FrontEnd</h2>
-    <a href="cad_user.html">Cadastrar Usuário</a>
-    <a href="vis_user.php.">Visualizar Usuário</a>
-
+    
 </body>
 </html>
 
