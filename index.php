5c6151ebd06fb112782d1e64d34584b657ce5aec
Davi Freire <davifreireangelim@gmail.com>
2021-06-17 21:35:38 -0300

    visual update

--- index.php
+++ index.php
@@ -5,14 +5,46 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto ALUNO</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body,h1 {font-family: "Raleway", sans-serif}
+h1{
+    color: red;
+}
+a {
+    color: red;
+}
+body, html {height: 100%}
+.bgimg {
+  background-image: url('img/uiui.jpg');
+  min-height: 100%;
+  background-position: center;
+  background-size: cover;
+}
+</style>
+
 </head>
 <body>
-    <?php
-        echo "Banco de dados com propósito de inserir alunos e suas notas <br>";
-    ?>
+<div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
+  <div class="w3-display-topleft w3-padding-large w3-xlarge">
+  
+        <h1>Projeto AV2 com objetivo de cadastrar alunos e suas notas.</h1>
+  
+  </div>
+  <div class="w3-display-middle">
+  <a class="w3-jumbo w3-animate-top" href="crud/create.php">Cadastrar aluno</a> 
+    <hr class="w3-border-grey" style="margin:auto;width:40%">
+    
+  </div>
+  <div class="w3-display-bottomleft w3-padding-large">
+    Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a>
+  </div>
+</div>
+    
 
-    <h2>Comandos</h2>
-    <a href="crud/create.php">Cadastrar aluno</a>
+    
     
     
 </body>
